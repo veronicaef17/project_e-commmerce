e9647cf344083a0ae02c0aa826cbb5c6734f147c
veronicaef17 <veronicaef17@gmail.com>
2023-08-10 13:31:05 +0700

    coba

---
+++ controllers/C_koneksi.php
@@ -0,0 +1,19 @@
+<?php
+// - buatkan kelas sesuai dengan nama file
+// - didalamnya terdapat sebuah fungsi yang bernama connection 
+
+// ini adalah kelas, nama kelas harus sama persis dengan nama file 
+class C_koneksi{
+
+    // ini ada fungsi atau method yang bernama connection dan fungsi harus ada didalam kelas 
+    public function connection(){
+        // untuk isinya kita lanjutkan hari rabu 
+    }
+
+}
+
+
+
+?>
+
+
